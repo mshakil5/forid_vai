204abb0fdb847295a0d326ac61dcad289282c6fb
shakil <kazimuhammadullah@gmail.com>
2025-03-08 01:24:22 +0600

    design change

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -14,12 +14,20 @@
             <!-- Profile Picture -->
             <div class="d-flex justify-content-center">
               <img src="{{asset('images/company/'.$profile->logo)}}" alt="Profile Image" class="profile-img shadow p-3">
+
+              
             </div>
+          </div>
+          <div class=" p-3">
+            <div class="profile-title" style="font-family: 'Times New Roman', Times, serif; font-weight: bold;">মুহাম্মদ ফরিদ হাসান</div>
+
+              {!! Str::before($profile->about_us, '</p>') !!}
+
           </div>
         </div>
         <div class="col-md-5 p-2">
             <div class=" p-3">
-            <div class="profile-title" style="font-family: 'Times New Roman', Times, serif; font-weight: bold;">মুহাম্মদ ফরিদ হাসান</div>
+            {{-- <div class="profile-title" style="font-family: 'Times New Roman', Times, serif; font-weight: bold;">মুহাম্মদ ফরিদ হাসান</div> --}}
                 {{-- <h3>About Me</h3>
                 <p id="about-me">I am a passionate editor with experience in writing and content creation. I love crafting stories and refining content to perfection.</p>
                 <h5>Contact Info</h5>
@@ -29,9 +37,15 @@
                 </ul> --}}
 
 
-                {!! Str::before($profile->about_us, '</p>') !!}
+                {{-- {!! Str::before($profile->about_us, '</p>') !!} --}}
 
-                <div class="row justify-content-center ">
+                <div class="row justify-content-center pt-4">
+                  <div class="col-12 col-md-4 profile-buttons p-2">
+                    <a href="{{route('research')}}" class="btn btn-danger">Research</a>
+                  </div> 
+                  <div class="col-12 col-md-4 profile-buttons p-2">
+                    <a href="{{route('essay')}}" class="btn btn-secondary">Essay</a>
+                  </div> 
                     <div class="col-12 col-md-4 profile-buttons p-2">
                       <a href="{{route('stories')}}" class="btn btn-success">Stories</a>
                     </div> 
@@ -39,13 +53,11 @@
                       <a href="{{route('poetries')}}" class="btn btn-warning">Poetry</a>
                     </div> 
                     <div class="col-12 col-md-4 profile-buttons p-2">
-                      <a href="{{route('essay')}}" class="btn btn-secondary">Essay</a>
+                      <a href="{{route('book')}}" class="btn btn-info">Book</a>
                     </div> 
+                    
                     <div class="col-12 col-md-4 profile-buttons p-2">
-                      <a href="{{route('research')}}" class="btn btn-danger">Research</a>
-                    </div> 
-                    <div class="col-12 col-md-4 profile-buttons p-2">
-                      <a href="{{route('book')}}" class="btn btn-success">Book</a>
+                      <a href="#" class="btn btn-dark" style="font-size: 13px;">International Publications</a>
                     </div> 
                     {{-- <div class="col-12 col-md-4 profile-buttons p-2">
                       <a href="{{route('stories')}}" class="btn btn-success">Stories</a>
@@ -55,6 +67,8 @@
             </div>
         </div>
       </div>
+
+
     </div>
   </main>
 
