3a63fc0b2fcfd32fa1eed2c37ee4eb99ef295124
shakil <kazimuhammadullah@gmail.com>
2025-03-08 01:30:15 +0600

    essay

--- resources/views/frontend/essay.blade.php
+++ resources/views/frontend/essay.blade.php
@@ -4,59 +4,53 @@
     
 @php
 $profile = \App\Models\CompanyDetail::select('company_name', 'position', 'about_us','logo')->first();
+$stories = \App\Models\Essay::select('id', 'description', 'name','feature_image','short_description')->get();
 @endphp
 <!-- Content Section -->
 <main class="content">
   
 
-  <div class="container py-5">
+<div class="page-container py-2">
     
-    <div class="row align-items-center  justify-content-center py-4">
-        <div class="col-md-4">
-            <h2 class="fw-bold">Project Name 01</h2>
-            <p>
-                I'm a paragraph. Click here to add your own text and edit me. It's easy. 
-                Just click “Edit Text” or double click me to add your own content and make changes to the font. 
-                I’m a great place for you to tell a story and let your users know a little more about you.
-            </p>
-        </div>
-        <div class="col-md-1"></div>
-        <div class="col-md-4">
-            <img src="{{asset('images/company/'.$profile->logo)}}" alt="Project Image" class="img-fluid">
-        </div>
-    </div>
-    
-    <div class="row align-items-center  justify-content-center py-4">
-        <div class="col-md-4">
-            <h2 class="fw-bold">Project Name 01</h2>
-            <p>
-                I'm a paragraph. Click here to add your own text and edit me. It's easy. 
-                Just click “Edit Text” or double click me to add your own content and make changes to the font. 
-                I’m a great place for you to tell a story and let your users know a little more about you.
-            </p>
-        </div>
-        
-        <div class="col-md-1"></div>
 
-        <div class="col-md-4">
-            <img src="{{asset('images/company/'.$profile->logo)}}" alt="Project Image" class="img-fluid">
-        </div>
+    @foreach ($stories as $story)
+    <div class="row align-items-center justify-content-center py-4 {{ $loop->iteration % 2 == 0 ? 'bg-light' : '' }}">
+        @if ($loop->iteration % 2 == 0)
+            <div class="col-md-4">
+                @if ($story->feature_image)
+                    <img src="{{asset('images/products/'.$story->feature_image)}}" alt="Project Image" class="img-fluid">
+                @endif
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-7">
+                <h2 class="fw-bold title">
+                    <a href="#">{{$story->name}}</a>
+                </h2>
+                <p>
+                    {!! Str::before($story->description, '</p>') !!}
+                </p>
+            </div>
+        @else
+            <div class="col-md-7">
+                <h2 class="fw-bold">{{$story->name}}</h2>
+                <p>
+                    {!! Str::before($story->description, '</p>') !!}
+                </p>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-4">
+                @if ($story->feature_image)
+                    <img src="{{asset('images/products/'.$story->feature_image)}}" alt="Project Image" class="img-fluid">
+                @endif
+            </div>
+        @endif
     </div>
+    @endforeach
     
-    <div class="row align-items-center  justify-content-center py-4">
-        <div class="col-md-4">
-            <h2 class="fw-bold">Project Name 01</h2>
-            <p>
-                I'm a paragraph. Click here to add your own text and edit me. It's easy. 
-                Just click “Edit Text” or double click me to add your own content and make changes to the font. 
-                I’m a great place for you to tell a story and let your users know a little more about you.
-            </p>
-        </div>
-        <div class="col-md-1"></div>
-        <div class="col-md-4">
-            <img src="{{asset('images/company/'.$profile->logo)}}" alt="Project Image" class="img-fluid">
-        </div>
-    </div>
+    
+    
+
+
 </div>
 
     
